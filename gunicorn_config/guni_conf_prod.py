556deca0b64a3323339561c6cb3284726aa5cc14
hachicomb <78338747+hachicomb@users.noreply.github.com>
2021-07-09 09:57:40 +0900

    Tweak

--- gunicorn_config/guni_conf_prod.py
+++ gunicorn_config/guni_conf_prod.py
@@ -6,7 +6,7 @@ worker_class = 'uvicorn.workers.UvicornWorker'
 bind = '127.0.0.1:8080'
 raw_env = ['MODE=PROD']
 daemon = True
-pidfile = './logs/prod_prod.pid'
+pidfile = './logs/prod.pid'
 errorlog = './logs/prod_error_log.txt'
 accesslog = './logs/prod_access_log.txt'
 loglevel = 'info'
--- gunicorn_config/guni_conf_staging.py
+++ gunicorn_config/guni_conf_staging.py
@@ -6,7 +6,7 @@ worker_class = 'uvicorn.workers.UvicornWorker'
 bind = '127.0.0.1:1080'
 raw_env = ['MODE=STAGING']
 daemon = True
-pidfile = './logs/staging_prod.pid'
+pidfile = './logs/staging.pid'
 errorlog = './logs/staging_error_log.txt'
 accesslog = './logs/staging_access_log.txt'
 loglevel = 'info'
